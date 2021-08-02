de1676e47cf4c2c95dfcf8ed63fbbd01c882911c
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2021-08-02 19:03:14 +0100

    Add proof of concept

---
+++ .eleventy.js
@@ -0,0 +1,76 @@
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+
+const Hypher = require('hypher')
+const english = require('hyphenation.en-gb');
+
+const slugify = require('slugify');
+
+const hypher = new Hypher(english);
+
+const { JSDOM } = require('jsdom');
+
+const util = require('util');
+
+module.exports = config => {
+
+  const markdownItOptions = {
+    html: true,
+    typographer: true
+  };
+
+  const markdownItAnchorOptions = {
+    permalink: true,
+    slugify: s => slugify(s, {
+      lower: true,
+      strict: true
+    }),
+    permalinkClass: "direct-link",
+    permalinkSymbol: "#",
+    permalinkAttrs: (slug, state) => ({
+      "aria-label": "Permalink: " + slug
+    }),
+    level: [1,2,3,4]
+  };
+
+  config.setLibrary(
+    "md",
+    markdownIt(markdownItOptions)
+      .use(markdownItAnchor, markdownItAnchorOptions)
+  );
+
+  config.setDataDeepMerge(true);
+
+  config.addTransform("hyphenation", (content, outputPath) => {
+
+    const dom = new JSDOM(content);
+    const document = dom.window.document;
+    const NodeFilter = dom.window.NodeFilter;
+
+    const walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
+
+    while (walk.nextNode()) {
+      walk.currentNode.nodeValue = hypher.hyphenateText(walk.currentNode.nodeValue);
+    }
+    return dom.serialize();
+  });
+
+  config.addFilter("slug", s =>
+    s !== undefined ?
+      slugify(s, { lower: true, strict: true }) :
+      "-"
+  );
+
+  config.addFilter("inspect", (s, d) =>
+    util.inspect(s, {depth: d === undefined ? 2 : d})
+  );
+
+  return {
+    dir: {
+      input: "src/view/",
+      output: "_site",
+      markdownTemplateEngine: "njk"
+    }
+  };
+}
+
---
+++ src/view/index.md
@@ -0,0 +1,25 @@
+---
+layout: home
+title: Home
+---
+
+This is a site for guides for Exponential Idle.
+
+Here are almost all of the resources that are used in the Discord.
+This post will have the guides that were created by SneakyBuddy911 and
+Baldy, the simulator (sim) by Antharion, and the calculator by Eaux.
+If you have any questions, please feel free to ask.
+
+SneakyBuddy's has a [YouTube channel] with some guides there for a
+couple of the theories and some other things that you might like.
+
+These are the student and star optimizers, just input your
+information, and it will tell you where to put everything:
+ - [Star Calculator]
+ - [Student Calculator]
+ - [Phi\*Tau Calculator]
+
+[YouTube channel]:https://www.youtube.com/channel/UCABRn80riRHN8r_ovCYLjrQ
+[Star Calculator]: https://conicgames.github.io/exponentialidle/stars.html
+[Student Calculator]: https://conicgames.github.io/exponentialidle/students.html
+[Phi\*Tau Calculator]: https://docs.google.com/spreadsheets/d/1lz_V0rCr3usm1Wyqn2DEJdw6_7lhWIxXCYFmUbArV1c/edit#gid=1147118496
