03d91a7d55d7b39e3d8887937cc9a54f58d0e494
LE★Baldy <ben.mathis02@gmail.com>
2023-01-02 22:41:23 -0600

    blockquote split

---
+++ src/scripts/blockquote-copy.js
@@ -0,0 +1,36 @@
+function remove_char(text, char){
+  var index = text.indexOf(char);
+  while (index >= 0) {
+    switch(index){
+      case 0:
+        text = text.slice(1);
+        break;
+      case text.length:
+        text = text.slice(0,text.length-1);
+        break;
+      default:
+        text = text.substr(0,index) + text.substr(index+1, text.length);
+    }
+    index = text.indexOf(char);
+  }
+  return text
+}     
+
+window.onload = ()=>{
+  const blockquote_list = document.getElementsByTagName('blockquote');
+  for(blockquote of blockquote_list){
+    blockquote.innerHTML = blockquote.innerHTML.replaceAll(String.fromCharCode(0x00ad),"");
+  }
+
+}
+
+function copyText(id){
+  text = document.getElementById(id).innerText;
+  text = remove_char(text, String.fromCharCode(10));
+  try {
+    navigator.clipboard.writeText(text);
+    console.log('Copy Success: \n' + text);
+  } catch (err) {
+    console.log('Copy Failure: \n' + toString(err));
+  }
+}
