802a5e7ce2f1c4879ce2890a1803de0f4134417b
LE★Baldy <ben.mathis02@gmail.com>
2024-05-01 00:20:40 -0500

    Update init.js

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -64,21 +64,27 @@ window.onload = ()=>{
             let curr_tr = eqnarray_tr[j];
             console.log("curr_tr");
             console.log(curr_tr);
-            let inner_HTML = curr_tr.innerHTML
-            console.log("curr_tr.innerHTML");
-            console.log(inner_HTML);
-            const translates = [...inner_HTML.matchAll(/"translate\((.*?),(.*?)\)/g)]
-            console.log("translates")
-            console.log(translates)
-            for (let k = translates.length - 1; k >= 0; k--) {
-              inner_HTML = inner_HTML.replace(translates[k][1], +translates[k][1] + curr_offset);
+            let curr_tr_td = curr_tr.querySelectorAll('[data-mml-node="mtd"]');
+
+            for (let k = 0; k < curr_tr_td.length; k++) {
+              let curr_td = curr_tr_td[k]
+              const transform = curr_td.getAttribute('transform')
+              console.log("curr_td.transform");
+              console.log(transform);
+              const translate = [...transform.matchAll(/"translate\((.*?),(.*?)\)/g)][0]
+              console.log("curr_td.translate");
+              console.log(translate);
+              curr_td.transform = transform.replace(translate[0], translate[0].replace(translate[1], (+translate[1] + curr_offset).toString()));
+              console.log("curr_td.transform");
+              console.log(curr_td.getAttribute('transform'));
+              if (k == curr_tr_td.length - 1) {
+                curr_offset = +translate[1];
+                console.log("curr_offset");
+                console.log(curr_offset);
+              }
             }
-            console.log("translates")
-            console.log([...inner_HTML.matchAll(/"translate\((.*?),(.*?)\)/g)])
-            curr_offset = +translates[translates.length - 1][1];
-            console.log("curr_offset");
-            console.log(curr_offset);
-            new_tr_innnerHTML += inner_HTML;
+  
+            new_tr_innnerHTML += curr_tr.innerHTML;
             console.log("new_tr_innnerHTML");
             console.log(new_tr_innnerHTML);
             if (j > 0) {
