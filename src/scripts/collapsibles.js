5dc7507fe529d3574b05f680287f740e8ebb7402
LE★Baldy <ben.mathis02@gmail.com>
2024-11-09 01:06:26 -0600

    defining loop variable

--- src/scripts/collapsibles.js
+++ src/scripts/collapsibles.js
@@ -207,7 +207,7 @@ if ($('h2').length > 1) {
 }
 
 
-for (i = 0; i < coll.length; i++) {
+for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     const content = this.nextElementSibling;
