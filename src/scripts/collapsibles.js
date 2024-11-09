69af2b8fefdfa480bb1a7099eff3cf8cd5eeb3ef
LE★Baldy <ben.mathis02@gmail.com>
2024-11-08 21:44:15 -0600

    adding h2 support

--- src/scripts/collapsibles.js
+++ src/scripts/collapsibles.js
@@ -28,6 +28,7 @@ function skipped(classlist, id = "") {
   }
 }
 
+/*
 $("h3").each(function() { //add collapsible to every h3 and h4 with unique ids based on location
   const h3 = $(this);
   const h3texts = strRepl(h3.text());
@@ -51,6 +52,42 @@ $("h3").each(function() { //add collapsible to every h3 and h4 with unique ids b
     }
   });
 });
+*/
+
+$("h2").each(function() {
+  const h2 = $(this);
+  const h2_text = strRepl(h2.text());
+  h2.attr('id', h2_text);
+  h2.addClass('collapsible');
+
+  $("h3").each(function() {
+    const h3 = $(this);
+    if (h3.isBefore($(h2).nextEle("h2")) || $(h2).nextEle("h2").text() === "") {
+      if (!(h3.isAfter($(h2)))
+          || skipped(h3.attr('class'), h3.attr('id') !== undefined ? h3.attr('id'): "")) {
+        return false
+      }
+
+      const h3_text = strRepl((h3texts + h4.text()))
+      h3.attr('id', h3_text);
+      h3.addClass('collapsible');
+
+      $("h4").each(function() {
+        const h4 = $(this);
+        if (h4.isBefore($(h3).nextEle("h3")) || $(h3).nextEle("h3").text() === "") {
+          if (!(h4.isAfter($(h3)))
+              || skipped(h4.attr('class'), h4.attr('id') !== undefined ? h4.attr('id'): "")) {
+            return false
+          }
+          
+          const h4text = strRepl((h3texts + h4.text()))
+          h4.attr('id', h4text);
+          h4.addClass('collapsible');
+        } else {return false;}
+      });
+    } else {return false;}
+  });
+});
 
 arrcollap = [];
 $('.collapsible').each(function() {
@@ -71,6 +108,7 @@ $('.collapsible').each(function() {
   arrcollap.push('#' + ele.attr('id'))
 })
 
+/*
 $('h3').each(function(i, e) {
   $(this)
     .nextUntil(this.tagName)
@@ -100,6 +138,61 @@ $('h3').each(function(i, e) {
     }
   })
 })
+*/
+
+$('h2').each(function(i, e) {
+  $(this)
+    .nextUntil(this.tagName)
+    .wrapAll('<div class="content"/>');
+
+  const h2_content = $('.content').last();
+  var h3_inside = false;
+  $('h3').each(function(p, q) {
+    const h3 = $(this)
+    h3_id = '#' + h3.attr('id');
+    if ($.contains(h2_content[0], h3[0])) {
+      const h3_index = arrcollap.indexOf(h3_id)
+      const h3_next_collap = arrcollap[h3_index + 1];
+      if (h3_next_collap === undefined || h3_index === -1) {
+        $(this)
+          .nextUntil(skiplist[0])
+          .wrapAll("<div class='content'/>");
+      } else if (h3_next_collap.slice(1) === h3.next().attr('id')) {
+        $(this).removeClass('collapsible');
+        h3_inside = true;
+      } else {
+        $(this)
+          .nextUntil(h3_next_collap)
+          .wrapAll("<div class='content'/>");
+        h3_inside = true;
+      }
+
+      const h3_content = $('.content').last();
+      var h4_inside = false;
+      $('h4').each(function(p, q) {
+        const h4 = $(this)
+        h4_id = '#' + h4.attr('id');
+        if ($.contains(h3_content[0], h4[0])) {
+          const h4_index = arrcollap.indexOf(h4_id)
+          const h4_next_collap = arrcollap[h4_index + 1];
+          if (h4_next_collap === undefined || h4_index === -1) {
+            $(this)
+              .nextUntil(skiplist[0])
+              .wrapAll("<div class='content'/>");
+            return false
+          } else if (h4_next_collap.slice(1) === h4.next().attr('id')) {
+            $(this).removeClass('collapsible');
+          } else {
+            $(this)
+              .nextUntil(h4_next_collap)
+              .wrapAll("<div class='content'/>");
+          }
+          h4_inside = true;
+        } else if (h3_inside) {return false}
+      });
+    } else if (h3_inside) {return false}
+  });
+});
 
 var coll = $('.collapsible');
 for (i = 0; i < coll.length; i++) {
