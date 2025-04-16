6f31e427cabc600504fd8ffedb739544f7fe8d3a
LE★Baldy <ben.mathis02@gmail.com>
2025-04-16 18:53:50 -0500

    forgot to add the regex in

--- src/scripts/collapsibles.js
+++ src/scripts/collapsibles.js
@@ -29,7 +29,7 @@ function get_url_paths(url) {
 }
 
 function get_skiplist(url) {
-  if(["http://localhost:8000/", "https://exponential-idle-guides.netlify.app/"].includes(url)){return []}
+  if("http://localhost:8000/" == url || /^https\:\/\/[a-z\-\/0-9]*exponential\-idle\-guides\.netlify\.app\/(?![a-z\-\/0-9]+)/g.test(url)){return []}
   const path = get_url_paths(url);
   const s = full_skiplist[path[0]][path[1]];
   return s === undefined ? [] : [,...s];
